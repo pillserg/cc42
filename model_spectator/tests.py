4998db44aff784aca982e87d2edd440ccad7ecad
Piljavsky Sergey <pill@i.ua>
2011-03-22 16:59:39 +0200

    ticket:10 test cases

--- model_spectator/tests.py
+++ model_spectator/tests.py
@@ -1,5 +1,8 @@
 from tddspry.django import DatabaseTestCase
+from django.contrib.auth.models import User
+
 from cc42.model_spectator.models import ModelChange
+
 
 class Test_onModelChangeEntryMustBeAddeToDB(DatabaseTestCase):
         
@@ -7,11 +10,32 @@ class Test_onModelChangeEntryMustBeAddeToDB(DatabaseTestCase):
         self.assert_create(ModelChange, full_name='test')
     
     def test_read(self):
-        ModelChange.objects.create(full_name='test12345')
-        self.assert_read(ModelChange, full_name='test12345')
+        ModelChange.objects.create(name='test12345')
+        self.assert_read(ModelChange, name='test12345')
         
     def test_update(self):
-        MC = ModelChange(full_name='test12345')
+        MC = ModelChange(name='test12345')
         MC.save()
-        self.assert_update(MC, full_name='test54321')
+        self.assert_update(MC, name='test54321')
 
+    def test_other_model(self):
+        CHOICES = (
+                ('1', 'Created_or_Updated'),
+                ('2', 'Deleted'),
+            )
+
+        User.objects.create_user('test_user','test@user.user')
+        u = User.objects.get(name='test_user')
+        last_entry = ModelChange.objects.all()[0]
+        self.assert_equal(last_entry.name, u.__name__)
+        self.assert_equal(last_entry.status, '1')
+        u.name = 'test_user2'
+        u.save()
+        last_entry = ModelChange.objects.all()[0]
+        self.assert_equal(last_entry.name, u.__name__)
+        self.assert_equal(last_entry.status, '1')
+        u.delete()
+        last_entry = ModelChange.objects.all()[0]
+        self.assert_equal(last_entry.name, 'test_user2')
+        self.assert_equal(last_entry.status, '2')
+        

975a62a8ac5da884b3c59cfa221008213ead9543
Piljavsky Sergey <pill@i.ua>
2011-03-24 18:38:35 +0200

    ticket:17 context_processor foxed again

--- context_processors.py
+++ context_processors.py
@@ -1,4 +1,5 @@
+from django.conf import settings
+
 def settings_context_processor(request):
     """adds request instance to RequestContext"""
-    from cc42 import settings
     return {'settings':settings}

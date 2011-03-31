1b565ae95df25eaf1caba326a9c163b3ca4f70aa
Piljavsky Sergey <pill@i.ua>
2011-03-31 15:30:03 +0300

    ticket6: formSerialize()

--- media/js/initAjaxForm.js
+++ media/js/initAjaxForm.js
@@ -1,37 +1,23 @@
 $(document).ready(function(){
-form = $("#user_profile_edit");
-
+	
 	$("#user_profile_edit").submit(send_post);	
 	$("#status").html("Ready")
 	
-	
 	function toggle_ui(bool){
 		if (bool == true){
-			form.slideToggle("slow");
+			$("#user_profile_edit").slideToggle("slow");
 			$("#user_profile_edit *").attr("disabled", true);
-
-
 		}
 		else{
-			form.slideToggle("slow");
+			$("#user_profile_edit").slideToggle("slow");
 			$("#user_profile_edit *").attr("disabled", false);
 		}
 		
 	}
-	
-	
+
 	function send_post(){
 		$("#status").html("Sending");
-		var POST = {
-				other_contacts: form.attr("other_contacts").value,
-				skype: form.attr("skype").value,
-				jabber: form.attr("jabber").value,
-				email: form.attr("email").value,
-				bio: form.attr("bio").value,
-				date_of_birth: form.attr("date_of_birth").value,
-				last_name: form.attr("last_name").value,
-				name: form.attr("name").value
-				}
+		var POST = $('#user_profile_edit').formSerialize();
 				
 		toggle_ui(true);
 		
@@ -43,8 +29,6 @@ form = $("#user_profile_edit");
 					$("#status").html("Done")
 					//reinit datepicker 
 					initDatePicker();
-					//rebindform
-					
 					toggle_ui(false);
 				}
 			);
